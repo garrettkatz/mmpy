4a4e8d9b8ab9a5a659bcd13d5820692943ca06f3
garrettkatz <garrett.katz@gmail.com>
2023-06-02 16:01:08 -0400

    code cleanup

--- proof.py
+++ proof.py
@@ -68,19 +68,51 @@ def perform(rule, dependencies):
     variable_pairs = {(min(u,v), max(u,v)) for (u,v) in it.product(substitution.keys(), repeat=2)}
 
     # disjoint variable checks
-    inherited_disjoint = set()
+    inherited = set()
     for (u, v) in variable_pairs & rule.disjoint:
         # check that disjoint variable substitutions remain disjoint
         assert len(subvars[u] & subvars[v]) == 0, f"{rule.consequent.label}: $d {u} {v} violated by {substitution}"
         # inherit disjoint requirements on the substituted variables
         for (x, y) in it.product(subvars[u], subvars[v]):
-            inherited_disjoint.add((min(x, y), max(x, y)))
+            inherited.add((min(x, y), max(x, y)))
 
     # infer conclusion from the rule
     conclusion = substitute(rule.consequent.tokens, substitution)
 
     # return results
-    return conclusion, substitution, inherited_disjoint
+    return conclusion, substitution, inherited
+
+"""
+Conduct one step of a proof of claim
+Applies given rule to top of stack; modifies stack in place
+Returns the resulting proof step object
+"""
+def conduct(rule, stack, claim):
+
+    # get rule hypotheses
+    hypotheses = rule.floatings + rule.essentials
+
+    # short-circuit hypothesis-less rules
+    if len(hypotheses) == 0:
+        return ProofStep(tuple(rule.consequent.tokens), rule, {}, {})
+
+    # pop top of stack
+    split = len(stack) - len(hypotheses)
+    dependencies = stack[split:]
+    del stack[split:]
+
+    # get conclusion by applying rule to top of stack
+    conclusion, substitution, disjoint = perform(rule, dependencies)
+
+    # check against missing disjoint requirements
+    assert disjoint <= claim.disjoint, \
+        f"missing $d requirements: {disjoint} - {claim.disjoint} = {disjoint - claim.disjoint}"
+
+    # wrap dependencies in dictionary by hypothesis label
+    dependencies = {hyp.label: dep for (hyp, dep) in zip(hypotheses, dependencies)}
+
+    # return results of step
+    return ProofStep(conclusion, rule, dependencies, substitution)
 
 """
 claim: a rule object whose proof will be verified
@@ -99,59 +131,22 @@ def verify_proof(database, claim):
         print(f"\n{step}")
         print(stack)
 
-        # get corresponding statement in database
-        statement = database.statements[step_label]
+        # conduct next proof step
+        rule = db.rules[step_label]
+        proof_step = conduct(rule, stack, claim)
+        conclusion = proof_step.conclusion
 
-        # if step is a hypothesis of claim, push onto stack
-        if statement.tag in ("$f", "$e"):
+        # save new proof steps
+        if proof_step.conclusion not in proof_steps:
+            proof_steps[proof_step.conclusion] = proof_step
 
-            # create placeholder inference for hypothesis if not already done
-            conclusion = tuple(statement.tokens)
-            if conclusion not in proof_steps:
-                rule = Rule(statement, [], [], set(), set())
-                proof_steps[conclusion] = ProofStep(conclusion, rule, {}, {})
-
-            # push onto stack
-            stack.append(proof_steps[conclusion])
-
-        # if step is a rule, apply to top of stack
-        if statement.tag in ("$a", "$p"):
-
-            # look up rule, its consequent, and hypothesis labels
-            rule = db.rules[step_label]
-            consequent = statement.tokens
-            hypotheses = rule.floatings + rule.essentials
-
-            # pop dependencies, one per hypothesis
-            split = len(stack) - len(hypotheses)
-            stack, dependencies = stack[:split], stack[split:]
-
-            # apply rule
-            conclusion, substitution, disjoint = perform(rule, dependencies)
-
-            # check against missing disjoint requirements
-            assert disjoint <= claim.disjoint, \
-                f"missing $d requirements: {disjoint} - {claim.disjoint} = {disjoint - claim.disjoint}"
-
-            # only construct new proof step if not already done
-            if conclusion not in proof_steps:
-
-                # wrap dependencies in dictionary by hypothesis label
-                dependencies = {hyp.label: dep for (hyp, dep) in zip(hypotheses, dependencies)}
-
-                # construct and save proof step
-                proof_steps[conclusion] = ProofStep(conclusion, rule, dependencies, substitution)
-
-            # push step onto stack
-            stack.append(proof_steps[conclusion])
+        # push proof step onto stack
+        stack.append(proof_step)
 
     # check that original claim has been proved
     assert len(stack) == 1, f"non-singleton stack {stack} after proof"
     assert stack[0].conclusion == tuple(claim.consequent.tokens), \
            f"proved statement {' '.join(stack[0].conclusion)} does not match theorem {' '.join(claim.consequent.tokens)}"
-
-    print(f"\nend:")
-    print(stack)
 
     # return root of proof graph and dictionary of nodes
     return stack[0], proof_steps
@@ -166,7 +161,6 @@ def verify_compressed_proof(database, claim):
 
     # convert to integer pointers and save tagged steps
     A, U, Z = ord('A'), ord('U'), ord('Z')
-    tagged_steps = {}
     step_pointers = []
     pointer = 0
     for ordinal in ordinals:
@@ -177,125 +171,44 @@ def verify_compressed_proof(database, claim):
         elif ordinal < Z:
             pointer = 5 * pointer + (ordinal - U) + 1
         else:
-            tagged_steps[len(step_pointers) - 1] = step_pointers[-1]
-    tagged_keys = tuple(tagged_steps.keys())
+            step_pointers.append(-1) # indicates last step should be tagged
 
-    # print(step_pointers)
-    # print(tagged_steps)
-
-    # setup offsets into hypotheses, labels, and tagged steps
-    claim_hypotheses = claim.floatings + claim.essentials
-    m = len(claim_hypotheses)
-    n = len(step_labels)
-
-    # print(f"proving {claim.consequent.label} with m={m} hypotheses {[hyp.label for hyp in claim_hypotheses]}")
-    # print(f"and n={n} step labels {step_labels}")
-    # print(f"and proof string {proof_string}")
-    # print("converted to ints", {i+1: pointer+1 for (i,pointer) in enumerate(step_pointers)})
-    # print(f"with tagged steps {tagged_steps}")
-
-    chunks = [chr(ordinals[0])]
-    for i in range(1, len(ordinals)):
-        if ordinals[i-1] == Z: chunks.append("")
-        elif ordinals[i-1] < U and ordinals[i] != Z: chunks.append("")
-        chunks[-1] += chr(ordinals[i])
-    # for c,chunk in enumerate(chunks): print(c+1, chunk, step_pointers[c]+1)
-
-    # initialize stack of proof steps
+    # initialize proof stack
     stack = []
 
-    # initialize set of proof steps, indexed by conclusion (tupled for hashability)
-    proof_steps = {}
-
-    # initialize list of proved conclusions
-    tagged_proof_steps = []
+    # initialize buffer dereferenced by step pointers
+    proof_steps = []
+    # steps for claim hypotheses
+    for hypothesis in claim.floatings + claim.essentials:
+        conclusion, rule = tuple(hypothesis.tokens), database.rules[hypothesis.label]
+        proof_steps.append(ProofStep(conclusion, rule, {}, {}))
+    # step labels
+    proof_steps += step_labels
 
     # process each step in proof
     for step, pointer in enumerate(step_pointers):
 
-        # print(f"\nstep {step+1}, pointer {pointer+1} stack:")
-        # for i,inf in enumerate(stack): print(i, inf.rule.consequent.label, ' '.join(inf.conclusion)[:80], '...' if len(' '.join(inf.conclusion)) > 80 else '')
-        # print("and tagged proof_steps:")
-        # for i,inf in enumerate(tagged_proof_steps): print(i, tagged_keys[i]+1, inf.rule.consequent.label, ' '.join(inf.conclusion)[:80], '...' if len(' '.join(inf.conclusion)) > 80 else '')
+        # tag previous step if requested
+        if pointer < 0:
+            proof_steps.append(stack[-1])
 
-        # if step is a hypothesis of claim, push onto stack
-        if pointer < m:
-
-            # get corresponding statement
-            step_label = claim_hypotheses[pointer].label
-            statement = database.statements[step_label]
-            # print(f"using hypothesis {step_label}:", ' '.join(statement.tokens))
-
-            # create placeholder inference for hypothesis
-            conclusion = tuple(statement.tokens)
-            rule = Rule(statement, [], [], set(), set())
-            proof_steps[conclusion] = ProofStep(conclusion, rule, {}, {})
-
-            # push onto stack
-            stack.append(proof_steps[conclusion])
-
-            # save tagged steps
-            if step in tagged_steps:
-                tagged_proof_steps.append(proof_steps[conclusion])
-
-        # if step is a rule, apply to top of stack
-        elif pointer < m + n:
-
-            # get corresponding statement
-            step_label = step_labels[pointer - m]
-            statement = database.statements[step_label]
-
-            # look up rule, its consequent, and hypothesis labels
-            rule = db.rules[step_label]
-            hypotheses = rule.floatings + rule.essentials
-            # print(f"using rule {step_label}:", ' '.join(statement.tokens))
-            # print("with hypotheses:", [hyp.label for hyp in hypotheses])
-
-            # pop dependencies, one per hypothesis
-            split = len(stack) - len(hypotheses)
-            stack, dependencies = stack[:split], stack[split:]
-
-            # apply rule
-            conclusion, substitution, disjoint = perform(rule, dependencies)
-
-            # check against missing disjoint requirements
-            assert disjoint <= claim.disjoint, \
-                f"missing $d requirements: {disjoint - claim.disjoint}"
-
-            # wrap dependencies in dictionary by hypothesis label
-            dependencies = {hyp.label: dep for (hyp, dep) in zip(hypotheses, dependencies)}
-
-            # construct and save inference
-            proof_steps[conclusion] = ProofStep(conclusion, rule, dependencies, substitution)
-
-            # push inference onto stack
-            stack.append(proof_steps[conclusion])
-
-            # save tagged steps
-            if step in tagged_steps:
-                tagged_proof_steps.append(proof_steps[conclusion])
-
-        # if step was already proved, push corresponding tagged inference back onto the stack again
+        # otherwise handle current proof step
         else:
 
-            # get previously proved inference
-            inference = tagged_proof_steps[pointer - (m + n)]
+            # retrieve current step
+            proof_step = proof_steps[pointer]
 
-            # print(f"reusing ({pointer-(m+n)})th tagged previous step {tagged_keys[pointer - (m + n)]+1}: ", ' '.join(inference.conclusion))
+            # replace labels by associated step
+            if type(proof_step) is str:
+                proof_step = conduct(db.rules[proof_step], stack, claim)
 
-            # push back onto stack
-            stack.append(inference)
-
-        # print("concluded", ' '.join(stack[-1].conclusion)[:80], '...' if len(' '.join(stack[-1].conclusion)) > 80 else '')
-
+            # push current proof step onto stack
+            stack.append(proof_step)
 
     # check that original claim has been proved
     assert stack[0].conclusion == tuple(claim.consequent.tokens), \
            f"proved statement {' '.join(stack[0].conclusion)} does not match theorem {' '.join(claim.consequent.tokens)}"
     assert len(stack) == 1, f"non-singleton stack {stack} after proof"
-
-    # print(f"\nend:")
-    # print(stack)
 
     # return root of proof graph and dictionary of nodes
     return stack[0], proof_steps
