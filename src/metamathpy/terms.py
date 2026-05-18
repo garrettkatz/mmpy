9c8e98fc8376d93867bc763d8e9eaafd6f3e0c53
Garrett Katz <garrettkatz@users.noreply.github.com>
2026-05-18 16:13:39 -0400

    some progress on terms

--- src/metamathpy/terms.py
+++ src/metamathpy/terms.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # types for term elements
-RULE, POINTER, VARIABLE, SENTINEL = range(3)
+RULE, POINTER, VARIABLE, SENTINEL = range(4)
 
 class Term:
     def __init__(self, data):
@@ -10,10 +10,16 @@ class Term:
         """
         self.data = data
 
+    def __repr__(self):
+        return f"Term(\n{self.data.T}.T)"
+
     def __eq__(self, other):
         return np.array_equal(self.data, other.data)
 
     def substitute(self, substitution):
+        but substituting one variable with another should not create a pointer
+    
+    
         top_data = self.data.copy()
         all_data = [top_data]
         offset = len(self.data)
@@ -35,18 +41,19 @@ class TermManager:
         self.arities = tuple(len(rule.floatings) for rule in rules)
 
     def rule_term(self, rule_index, subterms):
+        but subterms that are variables should not create a pointer
         arity = self.arities[rule_index]
-        offsets = np.cumsum([arity] + [t.data.shape[0] for t in subterms[:-1]])
+        offsets = np.cumsum([arity+1] + [t.data.shape[0] for t in subterms[:-1]])
         types = np.array([RULE] + [POINTER]*arity)
-        values = np.concatenate([np.array(rule_index), offsets])
-        data = np.concatenate([types, values], axis=1)
+        values = np.concatenate([np.array([rule_index]), offsets])
+        data = np.vstack([types, values]).T
         return Term(np.concatenate([data] + [t.data for t in subterms], axis=0))
 
     def variable_term(self, variable_index):
-        return Term(np.array([[VARIABLE], [variable_index]]))
+        return Term(np.array([[VARIABLE, variable_index]]))
 
     def sentinel_term(self, sentinel_index):
-        return Term(np.array([[SENTINEL], [sentinel_index]]))
+        return Term(np.array([[SENTINEL, sentinel_index]]))
 
     def unify(self, t1, t2):
         # t1, t2 are currently token sequences, replace with terms
@@ -54,5 +61,21 @@ class TermManager:
         return False, {}
 
 if __name__ == "__main__":
-    pass
+
+    import src.metamathpy.setmm as ms
+    db = ms.load_pl()
+    rules = db.rules_up_to("mp2")
+    tm = TermManager([rule for rule in rules["wff"] if rule.consequent.tag == "$a"])
+
+    for r, rule in enumerate(tm.rules): print(r); print(rule)
+
+    # t = Term(np.array([[1,2],[3,3]]))
+    # print(t)
+
+    s = tm.sentinel_term(0)
+    t = tm.variable_term(0)
+    print(t)
+    
+    t2 = tm.rule_term(1, [s, t])
+    print(t2)
 
